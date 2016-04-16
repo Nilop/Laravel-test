2d7cba7acee052d5e0b019a6c2353e1c188e0831
Nilop <NI.Lopatin@gmail.com>
2016-04-16 10:49:17 +0300

    first commit

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Category;
+
+class CategoriesController extends Controller
+{
+    public function create(){
+        return view('admin.categories.create');
+    }
+
+    public function store(Request $request){
+        Category::create($request->all());
+        //return back()->with('message', 'Категория добавлена');
+        return view('admin.categories.create', ['message'=>'Категория добавлена']);
+    }
+
+    public function update(Request $request, $id){
+        $category = Category::find($id);
+        $category->update($request->all());
+        $category->save();
+        return view('admin.categories.edit', ['message'=>'Категория отредактирована', 'category'=>$category]);
+
+
+    }
+
+    public function destroy($id){
+        $category = Category::find($id);
+        $category->delete();
+        return back()->with(['message'=>"Категория" . $category->title . " удалена"]);
+    }
+
+    public function edit($id){
+        $category = Category::find($id);
+        return view('admin.categories.edit', ['category'=>$category]);
+    }
+
+    public function index(){
+        $categories = Category::all();
+        return view('admin.categories.categories', ['categories'=>$categories]);
+    }
+
+}
